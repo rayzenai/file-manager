5d8763baafaad91ab435e30c61bebe828da2b126
timsinakiran <timsinakiran@gmail.com>
2025-01-21 12:38:46 +0545

    format

--- resources/views/media-page.blade.php
+++ resources/views/media-page.blade.php
@@ -11,11 +11,11 @@
 <body>
     <div class="flex  justify-center w-full">
         <img style="
-  /* Center and scale the image nicely */
-  background-position: center;
-  background-repeat: no-repeat;
-   object-fit: cover;
-  background-size: contain;"
+                /* Center and scale the image nicely */
+                background-position: center;
+                background-repeat: no-repeat;
+                object-fit: cover;
+                background-size: contain;"
             class="sm:w-full md:w-[1600px]" src="{{ $img }}" />
     </div>
 
